de35b01e4f60f54d797a8bd6e54808fd63d101eb
ProfessorMyk <116184964+ProfessorMyk@users.noreply.github.com>
2022-11-24 07:51:46 +0800

    Update RunAnimation.java

--- RunAnimation.java
+++ RunAnimation.java
@@ -4,11 +4,39 @@
  * and open the template in the editor.
  */
 package runanimation;
+import java.util.Scanner;
 
+public class RunAnimation {
 
-public abstract class Animal {
-    public abstract void eat();
-    public abstract void sleep();
-    public abstract void makeSound();
+    public static void main(String[] args) {
+        String choice;
+        Bird b = new Bird();
+        Cat c = new Cat();
+        Dog d = new Dog();
+        
+        Scanner num = new Scanner(System.in);
+        
+        System.out.println("Choose an animal. Press B for bird, C for cat, D for dog: ");
+        choice = num.nextLine();
+        
+        if (choice.equalsIgnoreCase("B")){
+            b.eat();
+            b.sleep();
+            b.makeSound();
+        }
+        else if (choice.equalsIgnoreCase("C")){
+            c.eat();
+            c.sleep();
+            c.makeSound();
+        }
+        else if (choice.equalsIgnoreCase("D")){
+            d.eat();
+            d.sleep();
+            d.makeSound();
+        }
+        else {
+            System.out.println("Invalid input! ");
+        }
+    }
     
 }
